5f4a6b2a6d6bde41f7b573535c467dd1f0ce45dd
Min Visible <minvisible842@gmail.com>
2026-04-01 13:39:04 +0300

    added code to first commit

--- python_file.py
+++ python_file.py
@@ -1,1 +1,9 @@
 print("Hello this is my first commit")
+# Declare variable `house_0_list`
+house_0_list = [115910.26, 128, 4]
+# Print object type of `house_0_list`
+print("house_0_list type:", type(house_0_list))
+# Print length of `house_0_list`
+print("house_0_list length:", len(house_0_list))
+# Get output of `house_0_list`
+house_0_list
